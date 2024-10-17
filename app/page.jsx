b7c2ec2d38055fc200205b289fed9096fbc3f882
gads131 <yosuabastian120@gmail.com>
2024-10-17 23:42:24 +0800

    second commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import yosua from "./assets/yosua.jpg"
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
       <div className="grid grid-cols-3 grid-rows-2 gap-4 p-4 max-w-4xl">
-        <div className="col-span-2 row-span-1 bg-white text-black p-4 rounded-lg">
+        <div className="col-span-2 row-span-1 bg-neutral-600 text-slate-50 p-4 rounded-lg">
           <p>
             Hi my name is Yosua Bastian Pitoy, I am from Tomohon Sulawesi Utara.
             Graduated with a bachelor degree in Informatic Engineering. Currently
@@ -13,19 +14,25 @@ export default function Home() {
             and performing monthly rekapitulation.
           </p>
         </div>
-        <div className="col-span-1 row-span-1 flex items-center justify-center bg-white">
+        <div className="col-span-1 row-span-1 items-start rounded-lg p-4 bg-neutral-600">
           <Image
-            src="/profile-picture.jpg"
+            src="/images/yosua.jpg"
             alt="Profile Picture"
-            width={100}
-            height={100}
-            className="rounded-full"
+            width={50}
+            height={50}
+            className="rounded-full bg-cover bg-"
           />
+          <p className="text-slate-50">
+            Tomohon, Sulawesi Utara 🇮🇩
+          </p>
         </div>
-        <div className="col-span-1 row-span-1 bg-white text-black p-4 rounded-lg">
+        <div className="col-span-1 row-span-10 bg-neutral-600 text-black p-4 rounded-lg">
           {/* Konten tambahan */}
         </div>
-        <div className="col-span-2 row-span-1 bg-white text-black p-4 rounded-lg">
+        <div className="col-span-2 row-span-10 bg-neutral-600 text-black p-4 rounded-lg">
+          {/* Konten tambahan */}
+        </div>
+        <div className="col-span-3 row-span-10 bg-neutral-600 text-black p-4 rounded-lg">
           {/* Konten tambahan */}
         </div>
       </div>
